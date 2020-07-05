17e8fba68379e6639cc8558851fbb93855aa90ba
Taiga <taiga248@gmail.com>
2020-07-05 18:57:04 +0900

    Update: app title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    publicPath: '/ramen_timer',
-    outputDir: 'docs'
-}
+  publicPath: "/ramen_timer",
+  outputDir: "docs",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "Ramen Timer"
+    }
+  }
+};
